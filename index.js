c9268e58ec7a6593e66eace9ceab10bde33745f9
Gustavo Sanchez <gustavosan_@hotmail.com>
2021-11-06 18:57:07 -0600

    Add script files

---
+++ index.js
@@ -0,0 +1,80 @@
+const { doRequest, encodeQueryData } = require("./services/APIService");
+const cliProgress = require("cli-progress");
+const fs = require("fs");
+const path = require("path");
+
+// Random Earth API Base URL
+const RandomEarthBaseURL = "https://randomearth.io/api/items";
+// Fill it with the collection NFT contract
+const CollectionAddress = "terra1whyze49j9d0672pleaflk0wfufxrh8l0at2h8q";
+const ProgressBar = new cliProgress.SingleBar(
+  {},
+  cliProgress.Presets.shades_classic
+);
+
+(async () => {
+  let totalPages = -1;
+  let currentPage = 1;
+  let RESnapshot = {};
+
+  console.log(
+    `Taking Owners snapshot from: ${CollectionAddress}`
+  );
+  do {
+    const getQuery = {
+      collection_addr: CollectionAddress,
+      page: currentPage,
+    };
+    const url = `${RandomEarthBaseURL}?${encodeQueryData(getQuery)}`;
+    try {
+      const data = await doRequest(url);
+      // Set total pages and progress bar on first execution
+      if (totalPages === -1) {
+        totalPages = data.pages;
+
+        // Create progress bar
+        ProgressBar.start(totalPages, 1);
+      }
+
+      for (const item of data.items) {
+        // If owner is not found create new record
+        if (!RESnapshot.hasOwnProperty(item.user_addr)) {
+          const SnapshotRecord = {
+            owner: item.user_addr,
+            tokens_found: 1,
+            token_ids: [item.token_id],
+          };
+          RESnapshot[SnapshotRecord.owner] = SnapshotRecord;
+        }
+        // If owner found just update data
+        else {
+          RESnapshot[item.user_addr].tokens_found++;
+          RESnapshot[item.user_addr].token_ids.push(item.token_id);
+        }
+      }
+      currentPage++;
+      ProgressBar.update(currentPage);
+    } catch (err) {
+      console.error(err);
+      sleep(err);
+    }
+  } while (currentPage <= totalPages);
+
+  ProgressBar.stop();
+  // Convert object to array
+  let SnapshotArray = Object.values(RESnapshot);
+
+  // Sort by token numbers
+  SnapshotArray = SnapshotArray.sort(function (a, b) {
+    return a.tokens_found < b.tokens_found ? -1 : 1;
+  });
+
+  const SnapshotFileName = "./snapshot.json";
+  // Create JSON file
+  fs.writeFileSync(SnapshotFileName, JSON.stringify(SnapshotArray));
+  const SnapshotAbsolutePath = path.resolve(SnapshotFileName);
+  const now = new Date();
+
+  console.log(`Snapshot taken sucessfully at ${now}`);
+  console.log(`You can check the snapshot here: ${SnapshotAbsolutePath}`);
+})();
---
+++ services/APIService.js
@@ -0,0 +1,32 @@
+const puppeteer = require("puppeteer-extra");
+const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+puppeteer.use(StealthPlugin());
+
+const doRequest = async (url) => {
+  const browser = await puppeteer.launch({
+    ignoreHTTPSErrors: true,
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
+  const page = await browser.newPage();
+  const response = await page.goto(url);
+  const data = await response.json();
+  await browser.close();
+  return data;
+};
+
+const encodeQueryData = (data) => {
+  const ret = [];
+  for (let d in data)
+    ret.push(encodeURIComponent(d) + "=" + encodeURIComponent(data[d]));
+  return ret.join("&");
+};
+
+const sleep = (ms) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+};
+
+exports.doRequest = doRequest;
+exports.encodeQueryData = encodeQueryData;
+exports.sleep = sleep;
